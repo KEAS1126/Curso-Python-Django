230c4dc052fd0e4887b02c9c59af33d8696e2440
KEAS1126 <keas11262000@gmail.com>
2025-11-10 19:45:26 -0500

    Finalizacion web persona - proyecto basico

--- webPersonal/portfolio/models.py
+++ webPersonal/portfolio/models.py
@@ -2,9 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title= models.CharField(max_length=200)
-    decription= models.TextField()
-    image= models.ImageField()
-    created=models.DateTimeField(auto_now_add=True)
-    updated=models.DateTimeField(auto_now=True)
+    title= models.CharField(max_length=200, verbose_name="Titulo")
+    decription= models.TextField(verbose_name="Descripcion")
+    image= models.ImageField(verbose_name="imagen", upload_to="projects")
+    link=models.URLField(verbose_name="Direccion web", null=True, blank=True)
+    created=models.DateTimeField(auto_now_add=True, verbose_name="fecha de creacion")
+    updated=models.DateTimeField(auto_now=True, verbose_name="ultima actualizacion")
+
+    class Meta:
+        verbose_name="Proyecto"
+        verbose_name_plural="Proyectos"
+        ordering=["-created"]
+    
+    def __str__(self):
+        return self.title
+        
 
--- webPersonal/webPersonal/settings.py
+++ webPersonal/webPersonal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'portfolio'
+    'portfolio.apps.PortfolioConfig'
 ]
 
 MIDDLEWARE = [
@@ -116,7 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+#Media files
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
